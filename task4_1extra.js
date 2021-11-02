0054322d73949e6505b199a1963ba1462d74c37d
MariBach <88796884+MariBach@users.noreply.github.com>
2021-11-02 17:32:45 +0200

    Add files via upload

---
+++ task4_1extra.js
@@ -0,0 +1,28 @@
+import Animals from './animals.js';
+import Mammals from './mammals.js';
+import Birds from './birds.js';
+import Fish from './fish.js'; 
+let rabbit = new Animals("Rabbit", "Mammals", "Herbivovers", 15);
+let wolf = new Mammals("Wolf", "Mammals", "Carnivores", 17);
+let sparrow = new Birds("Sparrow", "Birds", "Seed-eating", 20);
+let tuna = new Fish("Tuna", "Fish", "Carnivores", "Ocean", 70)
+Animals.displayBreeth();
+rabbit.displayAbility();
+console.log('--------------------------------------------');
+wolf.displayInfo();
+wolf.displayAbility();
+console.log('--------------------------------------------');
+sparrow.displayInfo();
+sparrow.displayUnique();
+sparrow.displayAbility();
+console.log('--------------------------------------------');
+tuna.displayInfo();
+tuna.displayUnique();
+tuna.displayAbility();
+
+    
+
+
+    
+
+
